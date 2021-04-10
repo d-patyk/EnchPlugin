6c11d1b0ead02be7594bffa3b64c7d7be3980891
Dmitriy Patyk <dima.compwizard@gmail.com>
2021-04-10 19:53:04 +0300

    First update :3

--- src/main/java/su/hil/enchantfixer/MyListener.java
+++ src/main/java/su/hil/enchantfixer/MyListener.java
@@ -3,6 +3,7 @@ package su.hil.enchantfixer;
 import me.dpohvar.powernbt.PowerNBT;
 import me.dpohvar.powernbt.api.NBTCompound;
 import org.bukkit.Material;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,11 +16,8 @@ import org.bukkit.inventory.PlayerInventory;
 
 
 public class MyListener implements Listener {
-    int i = 0; // DEBUG
     @EventHandler
     public void PlayerItemHeldEvent(PlayerItemHeldEvent playerItemHeldEvent) {
-        System.out.println(String.format("+event number %d", i++)); // DEBUG
-
         Player player = playerItemHeldEvent.getPlayer();
         PlayerInventory playerInventory = player.getInventory();
 
@@ -31,7 +29,7 @@ public class MyListener implements Listener {
             NBTCompound nbt = PowerNBT.getApi().read(item);
 
             if (nbt.containsKey("TokensHaste") && item.containsEnchantment(Enchantment.DIG_SPEED)) {
-                System.out.println("Ench removed from player ДКВ");
+                System.out.println(String.format("[EnchantFixer] Efficiency has been removed from player %s. Previous nbt: %s", player.getName(), nbt.toString()));
                 item.removeEnchantment(Enchantment.DIG_SPEED);
             }
         }
--- src/main/java/su/hil/enchantfixer/main.java
+++ src/main/java/su/hil/enchantfixer/main.java
@@ -5,8 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class main extends JavaPlugin {
     @Override
     public void onEnable() {
-        System.out.println("HIIIIIIIIIIIIIII, IMA PLUGINNNNN!N!N!NN!!");
         getServer().getPluginManager().registerEvents(new MyListener(), this);
+
+//        System.out.println("HIIIIIIIIIIIIIII, IMA PLUGINNNNN!N!N!NN!!");
     }
 
     @Override
